4c7abe4e2d84b01ecf98b18289ee4be8a87ffbb9
bruno dias <brunodiasdev@gmail.com>
2024-03-15 20:38:02 -0300

    fix: adjusting the config of eslint and updating the README

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: ['universe/native'],
+  rules: {
+    'no-undef': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
