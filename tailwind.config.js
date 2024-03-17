1d083dfcd17ea99fbe56e2a13bd156551982a1cf
bruno dias <brunodiasdev@gmail.com>
2024-03-17 06:19:01 -0300

    feat: setup the nativecn-ui and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-import { colors } from './src/styles/colors';
+import { colors } from './src/styles/colors'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  content: ["./src/**/*.{ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors,
+      colors
     },
   },
   plugins: [],
-};
+}
