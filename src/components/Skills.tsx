bc04d1a371378fd0e18f628cea2ae28df149a1cc
bruno dias <brunodiasdev@gmail.com>
2024-03-17 06:19:50 -0300

    feat: provide initital components for nativecn

---
+++ src/components/Skills.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Title } from "@/components/Title";
+
+export function Skills() {
+    return (
+        <View className="w-full">
+            <Title>Skills</Title>
+        </View>
+    )
+}
